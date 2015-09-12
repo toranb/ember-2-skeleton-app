bbb08e3a05c0672254ac98caf82cf54f9b8463c8
toran billups <toranb@gmail.com>
2015-09-12 10:35:49 -0500

    [STUB]: added simple server mocks to use the app locally

---
+++ server/mocks/users.js
@@ -0,0 +1,20 @@
+var users = require('../../vendor/fixtures/users.js');
+
+module.exports = function(app) {
+  var express = require('express');
+  var usersRouter = express.Router();
+
+  usersRouter.get('/', function(req, res) {
+    res.send(users.list());
+  });
+
+  usersRouter.put('/:id', function(req, res) {
+    res.send({
+      'users': {
+        id: req.params.id
+      }
+    });
+  });
+
+  app.use('/api/users', usersRouter);
+};
