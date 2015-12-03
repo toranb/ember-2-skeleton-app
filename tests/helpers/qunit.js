e972f06f894c77f424f5cda6ac714aa15cce0c16
toran billups <toranb@gmail.com>
2015-12-02 18:00:43 -0600

    updated to ember-cli 1.13.13 and ember 2.3.0-beta.2

--- tests/helpers/qunit.js
+++ tests/helpers/qunit.js
@@ -2,12 +2,23 @@ import Ember from 'ember';
 import {test} from 'qunit';
 import {module as qunitModule} from 'qunit';
 
+var Owner = Ember.Object.extend(Ember._RegistryProxyMixin, Ember._ContainerProxyMixin);
+
 var module = function(name, settings){
     settings = settings || {};
     qunitModule(name, {
         beforeEach: function() {
             this.registry = new Ember.Registry();
-            this.container = this.registry.container();
+
+            this.owner = Owner.create({
+                __registry__: this.registry
+            });
+
+            this.container = this.registry.container({
+                owner: this.owner
+            });
+
+            this.owner.__container__ = this.container;
             if(typeof settings.beforeEach === 'function') {
                 return settings.beforeEach.apply(this, arguments);
             }
@@ -15,6 +26,7 @@ var module = function(name, settings){
         afterEach: function() {
             this.container = null;
             this.registry = null;
+            this.owner = null;
             if(typeof settings.afterEach === 'function') {
                 return settings.afterEach.apply(this, arguments);
             }
--- tests/helpers/registration.js
+++ tests/helpers/registration.js
@@ -1,8 +1,8 @@
+import Resolver from 'ember-resolver';
 import Store from 'ember-cli-simple-store/store';
 
 export default function(container, registry, keys) {
-    var factory = window.require('ember/resolver')['default'];
-    var resolver = factory.create({namespace: {modulePrefix: 'admin'}});
+    var resolver = Resolver.create({namespace: {modulePrefix: 'admin'}});
     registry.register('store:main', Store);
     keys.forEach(function(key) {
         var factory = resolver.resolve('admin@' + key);
