3513d23b455d0233f8a154aa1b29e33dc2c771a4
toran billups <toranb@gmail.com>
2016-04-15 07:14:58 -0500

    fixed unit test break during the upgrade to ember 2.4

--- tests/unit/user-deserializer-test.js
+++ tests/unit/user-deserializer-test.js
@@ -1,5 +1,4 @@
-import QUnit from 'qunit';
-import {module} from 'admin/tests/helpers/qunit';
+import {module, test} from 'admin/tests/helpers/qunit';
 import users from 'admin/vendor/fixtures/users';
 import UserDeserializer from 'admin/deserializers/user';
 import registration from 'admin/tests/helpers/registration';
@@ -12,9 +11,8 @@ module('unit: user deserializer test', {
     }
 });
 
-QUnit.skip('api response deserialized into user models', (assert) => {
-    //TODO: how to create this object with the correct owner/container
-    var subject = UserDeserializer.create({store: store});
+test('api response deserialized into user models', (assert) => {
+    var subject = UserDeserializer.create({simpleStore: store});
     subject.deserialize(users.list());
     var models = store.find('user');
     assert.equal(models.get('length'), 3);
