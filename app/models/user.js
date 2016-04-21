25a5b29cbf963a76d2093df31375da12dfb7792a
toran billups <toranb@gmail.com>
2016-04-20 21:21:19 -0500

    fixed!

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,11 @@
+import Ember from 'ember';
 import { attr, Model } from 'ember-cli-simple-store/model';
 
 var User = Model.extend({
+    simpleStore: Ember.inject.service(),
+    wat: Ember.computed(function() {
+        return this.get('simpleStore').find('user');
+    }),
     name: attr(),
     serialize: function() {
         return {
--- tests/unit/user-deserializer-test.js
+++ tests/unit/user-deserializer-test.js
@@ -2,6 +2,7 @@ import {module, test} from 'admin/tests/helpers/qunit';
 import users from 'admin/vendor/fixtures/users';
 import UserDeserializer from 'admin/deserializers/user';
 import registration from 'admin/tests/helpers/registration';
+import User from 'admin/models/user';
 
 var store;
 
@@ -17,4 +18,20 @@ test('api response deserialized into user models', (assert) => {
     var models = store.find('user');
     assert.equal(models.get('length'), 3);
     assert.equal(models.objectAt(0).get('name'), users.list()[0].name);
+});
+
+test('toran wat', (assert) => {
+    store.push('user', {id: 1});
+    var user = store.findOne('user');
+    var models = store.find('user');
+    assert.equal(models.get('length'), 1);
+    assert.equal(user.get('wat').get('length'), 1);
+});
+
+test('toran wat2', (assert) => {
+    const user = User.create({simpleStore: store, id: 1});
+    store.push('user', {id: 1});
+    var models = store.find('user');
+    assert.equal(models.get('length'), 1);
+    assert.equal(user.get('wat').get('length'), 1);
 });
