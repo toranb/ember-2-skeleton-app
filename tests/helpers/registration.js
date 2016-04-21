452dfcffc72fd18740a4e7ed2e2dd8f57bab9709
toran billups <toranb@gmail.com>
2016-04-20 21:22:04 -0500

    updated test helper for simple-store v4

--- tests/helpers/registration.js
+++ tests/helpers/registration.js
@@ -3,10 +3,10 @@ import Store from 'ember-cli-simple-store/store';
 
 export default function(container, registry, keys) {
     var resolver = Resolver.create({namespace: {modulePrefix: 'admin'}});
-    registry.register('store:main', Store);
+    registry.register('service:simpleStore', Store);
     keys.forEach(function(key) {
         var factory = resolver.resolve('admin@' + key);
         registry.register(key, factory);
     });
-    return container.lookup('store:main');
+    return container.lookup('service:simpleStore');
 }
